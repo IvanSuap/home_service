ef21c4efa2398e7002ce0a72300338d4bd732243
IvanSuap <ivan.suap@gmail.com>
2020-04-23 18:48:54 -0500

    Main and SignUp Activityes

--- app/src/main/java/com/example/homeservice/SignUpActivity.java
+++ app/src/main/java/com/example/homeservice/SignUpActivity.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 
-    //Show SignUp view
-    public void ShowSignUp(View view){
-        Intent show = new Intent(this, SignUpActivity.class);
-        startActivity(show);
+    public void Back(View view){
+        Intent back = new Intent(this, MainActivity.class);
+        startActivity(back);
     }
 }
